ec5c35d08b6f4f45c5b9b5e6e1cb9c91f3e38ca5
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-03-04 19:01:00 +0400

    Added test case.

--- tests/unit/CZVatFormatValidatorTest.php
+++ tests/unit/CZVatFormatValidatorTest.php
@@ -30,8 +30,104 @@ class CZVatFormatValidatorTest extends TestCase
     {
         return [
             [
-                'vatNumber',
-                'isValid',
+                'vatNumber' => 'CZ12345678',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'CZ123456789',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'CZ1234567890',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '12345678',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '123456789',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '1234567890',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'cz12345678',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'Cz12345678',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'cZ12345678',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'cz123456789',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'cZ123456789',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'Cz123456789',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'cz1234567890',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'Cz1234567890',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'cZ1234567890',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'DE12345678',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'DE123456789',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'DE1234567890',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '1234567',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '12345678901',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '.12345678',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '.123456789',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '.1234567890',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '1',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '',
+                'isValid' => false,
             ],
         ];
     }
